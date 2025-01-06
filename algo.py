ca121a72a20ec35b1f48a074184b7fd599a2d3dc
Dronp2510 <dronpatel2510@gmail.com>
2025-01-06 23:22:48 +0530

    algo.py

--- algo.py
+++ algo.py
@@ -12,4 +12,16 @@ BUBBLE_SORT = [
     "   for j in range(n - i - 1):",
     "       if data[j] > data[j + 1]:",
     "           data[j], data[j + 1] = data[j + 1], data[j]"
-]   
+] 
+
+SELECTION_SORT = [
+        "for i in range(n):",
+        "   min_idx = i",
+        "   for j in range(i + 1, n):",
+        "       if self.data[j] < data[min_idx]:",
+        "           min_idx = j",
+]
+
+MERGE_SORT = [
+
+]
